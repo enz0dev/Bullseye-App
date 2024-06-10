569ececa77756ee40282f2d6c14f38d88d418d6f
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-06-10 17:10:21 +0100

    Add styling to text

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -19,10 +19,15 @@ struct ContentView: View {
         .font(.footnote)
         .kerning(2.0)
       Text("67")
+        .font(.largeTitle)
+        .kerning(-1)
+        .fontWeight(.black)
       HStack {
         Text("0")
+          .bold()
         Slider(value: .constant(50), in: 1.0...100.0)
         Text("100")
+          .bold()
       }
       Button("Hit me") {
         
