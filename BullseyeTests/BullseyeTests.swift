450af4c0e48f88b5d1d0a3f2bc3c6026f93bb9db
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-06-15 18:10:22 +0100

    Add unit test for game.points with positive score scenario

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -21,13 +21,14 @@ final class BullseyeTests: XCTestCase {
     game = nil
   }
   
-  func testExample() throws {
+  func testScorePositive() {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     // Any test you write for XCTest can be annotated as throws and async.
     // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
     // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    XCTAssertEqual(game.points(sliderValue: 50), 999)
+    var guess = game.target + 5
+    var score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
   }
-  
 }
