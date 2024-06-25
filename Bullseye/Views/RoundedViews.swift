358853886b57460988f1998b35d8d94eddcc68d7
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-06-25 22:52:41 +0100

    Add styling to buttons

--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -12,9 +12,30 @@ struct RoundedImageViewStroked: View {
   
   var body: some View {
     Image(systemName: systemName)
+      
       .font(.title)
-      .foregroundColor(Color("TextColor"))
+      .foregroundColor(Color("ButtonFilledTextColor"))
       .frame(width: 56, height: 56)
+      .overlay(
+        Circle()
+          .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0)
+        )
+  }
+}
+
+struct RoundedImageViewFilled: View {
+  var systemName: String
+  
+  var body: some View {
+    
+    Image(systemName: systemName)
+      .font(.title)
+      .foregroundColor(Color("ButtonFilledTextColor"))
+      .frame(width: 56, height: 56)
+      .background(
+        Circle()
+          .fill(Color("ButtonFilledBackgroundColor"))
+      )
   }
 }
 
