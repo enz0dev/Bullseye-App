825dc506045452afdeb3d3ae231aed03880e165d
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-10-16 21:44:20 +0100

    Add roundRectViewHeight constant to General in Constants

--- Bullseye/Models/Constants.swift
+++ Bullseye/Models/Constants.swift
@@ -12,5 +12,7 @@ enum Constants {
     public static let strokeWidth = CGFloat(2)
     public static let roundedViewLength = CGFloat(56)
     public static let roundRectViewWidth = CGFloat(68)
+    public static let roundRectViewHeight = CGFloat(68)
+    
   }
 }
