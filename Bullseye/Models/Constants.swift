0f33e8ad516d0f36a6f72749f0776f9ae1eef70f
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-10-16 21:41:19 +0100

    Add roundRectViewWidth constant to General in Constants

--- Bullseye/Models/Constants.swift
+++ Bullseye/Models/Constants.swift
@@ -11,5 +11,6 @@ enum Constants {
   enum General {
     public static let strokeWidth = CGFloat(2)
     public static let roundedViewLength = CGFloat(56)
+    public static let roundRectViewWidth = CGFloat(68)
   }
 }
