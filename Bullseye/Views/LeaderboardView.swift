7459c40bdc8ec1cb0a057b44e28135ae62937357
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-10-17 11:50:02 +0100

    Add: Include LeaderboardView.swift file in project

---
+++ Bullseye/Views/LeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardView.swift
+//  Bullseye
+//
+//  Created by Vince Harding on 17/10/2024.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  LeaderboardView()
+}
