90ee1a54dc04056c0cbe8ad481437fcd167e97dd
enz0dev <101827874+enz0dev@users.noreply.github.com>
2024-10-17 13:54:49 +0100

    Add RowView struct to LeaderboardView with placeholder Slider and styling

--- Bullseye/Views/LeaderboardView.swift
+++ Bullseye/Views/LeaderboardView.swift
@@ -13,6 +13,26 @@ struct LeaderboardView: View {
   }
 }
 
+struct RowView: View {
+  let index : Int
+  let score : Int
+  let date : Date
+  
+  var body: some View {
+    HStack {
+      Slider(value: .constant(50))
+        .padding()
+    }
+    .background(
+      RoundedRectangle(cornerRadius: .infinity)
+        .strokeBorder(Color("LeaderboardRowColor"), lineWidth: Constants.General.strokeWidth)
+    )
+    .padding(.horizontal)
+    .frame(maxWidth: 480)
+  }
+}
+
+
 #Preview {
   LeaderboardView()
 }
